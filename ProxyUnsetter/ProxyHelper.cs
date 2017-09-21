f133fd2fee89ec6260a85302e77c5f2b9a2128f6
Tjeerd Hans Terpstra <tjeerdhans@outlook.com>
2017-09-21 22:16:45 +0200

    Add project files.

---
+++ ProxyUnsetter/ProxyHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ProxyUnsetter
+{
+    internal static class ProxyHelper
+    {
+        [DllImport("wininet.dll")]
+        private static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+
+        // ReSharper disable InconsistentNaming
+        private const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        private const int INTERNET_OPTION_REFRESH = 37;
+        // ReSharper restore InconsistentNaming
+
+        //bool settingsReturn, refreshReturn;
+
+
+        public static bool GetCurrentProxyState()
+        {
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            if (registry == null)
+            {
+                MessageBox.Show(@"Couldn't find the registry key to update proxy settings.", @"Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return true;
+            }
+            var proxyEnableValue = registry.GetValue("ProxyEnable");
+            if (proxyEnableValue != null && (int)proxyEnableValue == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static void RefreshProxySettings()
+        {
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+        }
+
+        public static void UnsetProxy()
+        {
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            if (registry == null)
+            {
+                MessageBox.Show(@"Couldn't find the registry key to update proxy settings.", @"Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+            registry.SetValue("ProxyEnable", 0);
+            //registry.SetValue("ProxyServer", "127.0.0.1:8080");
+            RefreshProxySettings();
+        }
+
+        public static void SetProxy()
+        {
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            if (registry == null)
+            {
+                MessageBox.Show(@"Couldn't find the registry key to update proxy settings.", @"Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+            registry.SetValue("ProxyEnable", 1);
+            registry.SetValue("ProxyServer", "127.0.0.1:8080");
+            RefreshProxySettings();
+        }
+    }
+}
---
+++ ProxyUnsetter/ProxyUnsetterApplicationContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ProxyUnsetter.Properties;
+
+namespace ProxyUnsetter
+{
+    public class ProxyUnsetterApplicationContext : ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+
+        public ProxyUnsetterApplicationContext()
+        {
+            var trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Force unset proxy now (double click)", OnUnsetProxy);
+            trayMenu.MenuItems.Add("set", OnSetProxy);
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            _trayIcon = new NotifyIcon
+            {
+                Text = @"ProxyUnsetter",
+                Icon = SystemIcons.Asterisk,// proxySet ? Resources.SetProxy : Resources.UnsetProxy,
+                ContextMenu = trayMenu,
+                Visible = true
+            };
+
+            SetTrayIcon();
+
+            _trayIcon.DoubleClick += OnUnsetProxy;
+        }
+
+        private void SetTrayIcon()
+        {
+            var proxySet = ProxyHelper.GetCurrentProxyState();
+            _trayIcon.Icon = proxySet ? Resources.SetProxy : Resources.UnsetProxy;
+            _trayIcon.Text = $@"Proxy is {(proxySet ? "" : "not ")}set";
+        }
+
+        private void OnSetProxy(object sender, EventArgs e)
+        {
+            ProxyHelper.SetProxy();
+            SetTrayIcon();
+        }
+
+        private void OnUnsetProxy(object sender, EventArgs e)
+        {
+            ProxyHelper.UnsetProxy();
+            SetTrayIcon();
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                // Release the icon resource.
+                // Hide tray icon, otherwise it will remain shown until user mouses over it
+                _trayIcon.Visible = false;
+                _trayIcon.Dispose();
+            }
+
+            base.Dispose(isDisposing);
+        }
+    }
+}
