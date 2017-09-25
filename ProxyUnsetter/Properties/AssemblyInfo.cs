b148cbdb2ec392a6f00a5c96a18c45b6476240b5
Tjeerd Hans Terpstra <tjeerdhans@gmail.com>
2017-09-25 21:01:43 +0200

    Attribution for the logo and icons.

--- ProxyUnsetter/Properties/AssemblyInfo.cs
+++ ProxyUnsetter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ProxyUnsetter")]
-[assembly: AssemblyDescription("Tray application that will unset your proxy.")]
+[assembly: AssemblyDescription("Tray application that will unset your proxy.\n\nLogo and icons adapted from http://icons8.com/")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("THT")]
 [assembly: AssemblyProduct("ProxyUnsetter")]
