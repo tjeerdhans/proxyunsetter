31c25de4d6b600441b7c90d0e73f7e6f80f2163a
Tjeerd Hans Terpstra <tjeerdhans@gmail.com>
2017-11-19 13:09:19 +0100

    Removed default ip range whitelist.

--- ProxyUnsetter/Properties/Settings.Designer.cs
+++ ProxyUnsetter/Properties/Settings.Designer.cs
@@ -62,8 +62,7 @@ namespace ProxyUnsetter.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>172.16.101.14/255.255.254.0</string>\r\n</ArrayOfString>")]
+            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />")]
         public global::System.Collections.Specialized.StringCollection IpWhitelist {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["IpWhitelist"]));
