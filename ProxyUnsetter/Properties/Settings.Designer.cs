d427a664049ed79c2162f3182b7863bdaa41f147
Tjeerd Hans Terpstra <tjeerdhans@gmail.com>
2017-12-01 14:43:51 +0100

    Check for new release now defaults to True.

--- ProxyUnsetter/Properties/Settings.Designer.cs
+++ ProxyUnsetter/Properties/Settings.Designer.cs
@@ -49,7 +49,7 @@ namespace ProxyUnsetter.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool CheckForNewReleaseWeekly {
             get {
                 return ((bool)(this["CheckForNewReleaseWeekly"]));
